4d42d4c93679508cc3423e95b41a591c6226f9ee
GioCC <25667790+GioCC@users.noreply.github.com>
2022-02-06 13:44:57 +0100

    Checked templated AddItem: OK

--- include/MF_Inputs/MFInBase.h
+++ include/MF_Inputs/MFInBase.h
@@ -14,21 +14,20 @@
 class MFInBase
 {
     public:
-        // typedef enum {
-        //     NONE        = kTypeNotSet,
-        //     BUTTON      = kTypeButton,
-        //     ENCODER_SD  = kTypeEncoderSingleDetent, //deprecated
-        //     ENCODER     = kTypeEncoder,
-        //     ANALOG      = kTypeAnalogInput,
-        //     SHIFTREG    = kTypeInputShifter,
-        // } InType;
 
         MFInBase() {}
 
+        // Common methods like following should exist, however since it's 
+        // very difficult to define a common signature for the interface
+        // (or because they could not be made accessible when required, 
+        // like getSize()/getType/()), they're only listed here as a reminder.
+        // Each class will define the corresponding methods with their own signature.
+        
         // virtual uint8_t getSize(void)   = 0;
         // virtual uint8_t getType(void)   = 0;
-        // virtual void OnChange(void) = 0;
-        // virtual void Add(void) = 0;
+
+        // virtual void setup(...)         = 0;     // Assign connections upon creation
+        // virtual void attachHandler(...) = 0;     // When an input event occurs, manage it
         
         virtual void update(void)       = 0;
         virtual void onReset(void)      {};
--- src/inputHub.cpp
+++ src/inputHub.cpp
@@ -29,11 +29,10 @@ void SetInputHandlers(void)
 }
 
 // Generic form (the param init will use the returned pointer)
-template <typename T> T* AddItem(T** dummy, StowManager *SM)
+template <typename T> T* AddItem(T** itemPtr, StowManager *SM)
 {
-    // dummy argument required to set signature
-    // Or make it 'Add(T** res, ...)' and take advantage of it to 
-    // carry the return value 
+    // Since itemPtr argument is required to set signature anyway, 
+    // we take advantage of it to carry the return value 
     uint8_t *in;
     in = SM->add(T::getSize(), T::getType()); 
     if(in != NULL) {
@@ -107,9 +106,9 @@ void AddButton(uint8_t pin, char const *name)
 {
     MFButton *MFB;
     AddItem(&MFB);
-    // Non-templated alternative (uglier but probably less memory-consuming):
-    //MFB = (MFButton *)InStow.add(MFButton::getSize(), MFButton::getType());
-    //if(MFB) new ((void *)MFB) MFButton;     // Why does the linter not like this placement new?
+    // Non-templated alternative would be more memory-consuming (both RAM and flash!):
+    // MFB = (MFButton *)InStow.add(MFButton::getSize(), MFButton::getType());
+    // if(MFB) new ((void *)MFB) MFButton;
 
     MFB->setup(pin, name);
 
@@ -133,7 +132,9 @@ void OnEncoderChange(uint8_t eventId, uint8_t pin, const char *name)
 void AddEncoder(uint8_t pin1, uint8_t pin2, uint8_t encoder_type, char const *name)
 {
     MFEncoder *MFE;
+
     AddItem(&MFE);
+
     MFE->setup(pin1, pin2, encoder_type, name);
     #ifdef DEBUG
     cmdMessenger.sendCmd(kStatus, F("Added encoder"));
@@ -157,7 +158,9 @@ void OnAnalogChange(int value, uint8_t pin, const char *name)
 void AddAnalog(uint8_t pin, uint8_t sensitivity, char const *name)
 {
     MFAnalog *MFA;
+
     AddItem(&MFA);
+    
     MFA->setup(pin, sensitivity, name);
     #ifdef DEBUG
     cmdMessenger.sendCmd(kStatus, F("Added analog device "));
@@ -184,7 +187,9 @@ void OnInputShiftRegChange(uint8_t eventId, uint8_t pin, const char *name)
 void AddInputShiftReg(uint8_t latchPin, uint8_t clockPin, uint8_t dataPin, uint8_t nModules, char const *name)
 {
     MFInputShifter *MFI;
+
     AddItem(&MFI);
+
     MFI->setup(latchPin, clockPin, dataPin, nModules, name);
     #ifdef DEBUG
     cmdMessenger.sendCmd(kStatus, F("Added input shifter"));
