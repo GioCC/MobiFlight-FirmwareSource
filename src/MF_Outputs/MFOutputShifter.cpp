8652662afb7d21fdebbe9bbebc4b8beb8091c08d
GioCC <25667790+GioCC@users.noreply.github.com>
2022-02-22 21:29:25 +0100

    Output shifter fix #155

--- src/MF_Outputs/MFOutputShifter.cpp
+++ src/MF_Outputs/MFOutputShifter.cpp
@@ -37,7 +37,7 @@ void MFOutputShifter::update()
 {
     digitalWrite(_latchPin, LOW);
     for (uint8_t i = _moduleCount; i > 0; i--) {
-        shiftOut(_dataPin, _clockPin, MSBFIRST, _outputBuffer[i]); // LSBFIRST, MSBFIRST,
+        shiftOut(_dataPin, _clockPin, MSBFIRST, _outputBuffer[i-1]); // LSBFIRST, MSBFIRST,
     }
     digitalWrite(_latchPin, HIGH);
 }
