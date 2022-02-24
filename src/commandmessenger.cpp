d3b5b7c5468870e882673bb4cedfe44e2ab5f782
GioCC <25667790+GioCC@users.noreply.github.com>
2022-02-24 14:28:20 +0100

    Format change

--- src/commandmessenger.cpp
+++ src/commandmessenger.cpp
@@ -1,67 +1,64 @@
-#include <Arduino.h>
 #include "MFBoards.h"
 #include "mobiflight.h"
-
+#include <Arduino.h>
 
 CmdMessenger cmdMessenger = CmdMessenger(Serial);
 unsigned long lastCommand;
 
 // Prototypes for forward declarations
 static void OnUnknownCommand();
-//static void OnResetBoard();
-
+// static void OnResetBoard();
 
 // Callbacks define on which received commands we take action
 void attachCommandCallbacks()
 {
-  // Attach callback methods
-  cmdMessenger.attach(OnUnknownCommand);
+    // Attach callback methods
+    cmdMessenger.attach(OnUnknownCommand);
 
 #if MF_SEGMENT_SUPPORT == 1
-  cmdMessenger.attach(kInitModule, OnInitModule);
-  cmdMessenger.attach(kSetModule, OnSetModule);
-  cmdMessenger.attach(kSetModuleBrightness, OnSetModuleBrightness);
+    cmdMessenger.attach(kInitModule, OnInitModule);
+    cmdMessenger.attach(kSetModule, OnSetModule);
+    cmdMessenger.attach(kSetModuleBrightness, OnSetModuleBrightness);
 #endif
 
-  cmdMessenger.attach(kSetPin, OnSetPin);
+    cmdMessenger.attach(kSetPin, OnSetPin);
 
 #if MF_STEPPER_SUPPORT == 1
-  cmdMessenger.attach(kSetStepper, OnSetStepper);
+    cmdMessenger.attach(kSetStepper, OnSetStepper);
 #endif
 
 #if MF_SERVO_SUPPORT == 1
-  cmdMessenger.attach(kSetServo, OnSetServo);
+    cmdMessenger.attach(kSetServo, OnSetServo);
 #endif
 
-  cmdMessenger.attach(kGetInfo, OnGetInfo);
-  cmdMessenger.attach(kGetConfig, OnGetConfig);
-  cmdMessenger.attach(kSetConfig, OnSetConfig);                 // 3rd step, uploading new config block wise
-  cmdMessenger.attach(kResetConfig, OnResetConfig);             // 2nd step for uploading a new config
-  cmdMessenger.attach(kSaveConfig, OnSaveConfig);               // 4th step, not really required anymore, config is stored directly to EEPROM, changes in UI required as feedback is required for now
-  cmdMessenger.attach(kActivateConfig, OnActivateConfig);       // 5th step, reading config and activate
-  cmdMessenger.attach(kSetName, OnSetName);                     // 1st step, write name
-  cmdMessenger.attach(kGenNewSerial, OnGenNewSerial);
+    cmdMessenger.attach(kGetInfo, OnGetInfo);
+    cmdMessenger.attach(kGetConfig, OnGetConfig);
+    cmdMessenger.attach(kSetConfig, OnSetConfig);           // 3rd step, uploading new config block wise
+    cmdMessenger.attach(kResetConfig, OnResetConfig);       // 2nd step for uploading a new config
+    cmdMessenger.attach(kSaveConfig, OnSaveConfig);         // 4th step, not really required anymore, config is stored directly to EEPROM, changes in UI required as feedback is required for now
+    cmdMessenger.attach(kActivateConfig, OnActivateConfig); // 5th step, reading config and activate
+    cmdMessenger.attach(kSetName, OnSetName);               // 1st step, write name
+    cmdMessenger.attach(kGenNewSerial, OnGenNewSerial);
 
 #if MF_STEPPER_SUPPORT == 1
-  cmdMessenger.attach(kResetStepper, OnResetStepper);
-  cmdMessenger.attach(kSetZeroStepper, OnSetZeroStepper);
+    cmdMessenger.attach(kResetStepper, OnResetStepper);
+    cmdMessenger.attach(kSetZeroStepper, OnSetZeroStepper);
 #endif
 
-  cmdMessenger.attach(kTrigger, OnTrigger);
-//  cmdMessenger.attach(kResetBoard, OnResetBoard);               // why is this command coming from the UI additional to OnActivateConfig?
+    cmdMessenger.attach(kTrigger, OnTrigger);
+    //  cmdMessenger.attach(kResetBoard, OnResetBoard);               // why is this command coming from the UI additional to OnActivateConfig?
 
 #if MF_LCD_SUPPORT == 1
-  cmdMessenger.attach(kSetLcdDisplayI2C, OnSetLcdDisplayI2C);
+    cmdMessenger.attach(kSetLcdDisplayI2C, OnSetLcdDisplayI2C);
 #endif
 
 #if MF_OUTPUT_SHIFTER_SUPPORT
-  cmdMessenger.attach(kSetShiftRegisterPins, OnSetOutputShifterPins);
+    cmdMessenger.attach(kSetShiftRegisterPins, OnSetOutputShifterPins);
 #endif
 
 #ifdef DEBUG
-  cmdMessenger.sendCmd(kStatus, F("Attached callbacks"));
+    cmdMessenger.sendCmd(kStatus, F("Attached callbacks"));
 #endif
-
 }
 
 // uint32_t getLastCommandMillis() {
@@ -84,4 +81,3 @@ void OnUnknownCommand()
 //   resetConfig();        // was part of loadConfig(), but not needed on initial start up
 //   resetBoard();
 // }
-
