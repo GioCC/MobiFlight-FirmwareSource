61589bde53581e031e8d09efe635a387eea57304
GioCC <25667790+GioCC@users.noreply.github.com>
2022-12-20 13:41:43 +0100

    Mod complete (unless I missed something)

--- src/MF_DigInMux/MFDigInMux.cpp
+++ src/MF_DigInMux/MFDigInMux.cpp
@@ -13,19 +13,14 @@ MFMuxDriver *MFDigInMux::_MUX;
 MuxDigInEvent MFDigInMux::_inputHandler = NULL;
 
 MFDigInMux::MFDigInMux(void)
+    : _name("MUXDigIn"), _dataPin(0xFF), _flags(0), _lastState(0), _tempState(0)
 {
-    _MUX   = NULL;
-    _name  = "MUXDigIn";
-    _flags = 0x00;
-    clear();
 }
 
 MFDigInMux::MFDigInMux(MFMuxDriver *MUX, const char *name)
-    : _name(name)
+    : _name(name), _dataPin(0xFF), _flags(0), _lastState(0), _tempState(0)
 {
     if (MUX) _MUX = MUX;
-    _flags = 0x00;
-    clear();
 }
 
 void MFDigInMux::setMux(MFMuxDriver *MUX)
@@ -56,16 +51,26 @@ void MFDigInMux::detach()
     }
 }
 
-// Helper for "poll" function: does the input reading and triggering part,
-// relying on an external setting for the Mux channel
+// Helper for "poll" function: does the reading and triggering part
+// for a single (the current) input, relying on an external setting
+// for the Mux channel.
 // Extrapolated from "poll" so it can be used with different
-// mux address-setting strategies
-void MFDigInMux::pollNext(bool doTrigger, bool start)
+// mux address-setting strategies.
+// Scan MUST occur sequentially!
+// "pass" is the scan count, and MUST start from 0 upwards.
+// BEWARE: scanning the channels from 0 upwards, bits in _lastState
+// are in reverse order (bit 0 = channel 8 or 16, depending on mux type).
+// This has no consequence beside being counterintuitive, but must be kept in mind
+// in case of future modifications.
+// In fact, the channels between 0 and the [max input for the mux type]-1 (ie 7 or 15)
+// could be scanned in any order, as long as it is consistent between calls.
+void MFDigInMux::pollNext(bool doTrigger, uint8_t pass)
 {
-    const uint8_t    selMax = (bitRead(_flags, MUX_HALFSIZE) ? 8 : 16);
+    const uint8_t    selMax = (bitRead(_flags, MUX_HALFSIZE) ? 8 - 1 : 16 - 1);
     volatile uint8_t pinVal;
 
-    if (start) _tempState = 0x0000;
+    if (pass > selMax) return;
+    if (0 == pass) _tempState = 0x0000;
 
     // Allow the output to settle from voltage transients:
     // transients towards 0 (GND) are negligible, but transients towards 1 (Vcc)
@@ -74,20 +79,25 @@ void MFDigInMux::pollNext(bool doTrigger, bool start)
     // integrated PU -> 1.4us
     // external, 10k -> 400ns
     // external, 4k7 -> 250ns
-    // A digitalRead() takes about 5us, therefore even the integrated pullup should be sufficient.
+    // A digitalRead() takes about 5us (also the time for function calling must be accounted for),
+    // therefore even the integrated pullup should be sufficient.
     // NB An external pullup (10k or 4k7) is recommended anyway for better interference immunity.
-
     pinVal = digitalRead(_dataPin);
     _tempState <<= 1;
     _tempState |= (pinVal ? 1 : 0);
 
-    // if (doTrigger) {
-    //     if( detectChanges(_lastState, _currentState) _lastState = _currentState;
-    // }
+    // BEWARE: For optimization, the detection of changes (input triggering)
+    // is made only once - for all inputs - at the last pass.
+    // This means that:
+    // - selector indexes before the last can be scanned in any order, but all must be scanned before the last;
+    // - the last selector must be scanned (can't be skipped) in order to trigger change detection.
+    if (doTrigger && (pass == selMax)) {
+        if (detectChanges(_lastState, _tempState)) _lastState = _tempState;
+    }
 }
 
 // Core function for update() and retrigger()
-// Reads the values from the attached modules, compares them to the previously
+// Reads the values from the attached module, compares them to the previously
 // read values, and calls the registered event handler for any inputs that
 // changed from the previously read state.
 void MFDigInMux::poll(bool doTrigger)
@@ -95,17 +105,10 @@ void MFDigInMux::poll(bool doTrigger)
     if (!_MUX) return;
 
     uint8_t selMax = (bitRead(_flags, MUX_HALFSIZE) ? 8 : 16);
-    // uint16_t currentState = 0x0000;
-    // volatile uint8_t pinVal;
 
-    for (uint8_t sel = selMax; sel > 0; sel--) {
-        _MUX->setChannel(sel - 1);
-        pollNext(doTrigger, sel - 1);
+    for (uint8_t sel = 0; sel < selMax; sel++) {
+        pollNext(doTrigger, _MUX->setChannel(sel));
     }
-
-    // if (doTrigger) {
-    //     if( detectChanges(_lastState, _currentState) _lastState = _currentState;
-    // }
 }
 
 // Detects changes between the current state and the previously saved state
@@ -124,13 +127,12 @@ bool MFDigInMux::detectChanges(uint16_t lastState, uint16_t currentState)
     return true;
 }
 
-// Reads the current state for all connected modules then fires
-// 'release' events for every 'off' input, followed by
-// 'press' events for every 'on' input.
+// Reads the current state then fires 'release' events for every 'off' input,
+// followed by 'press' events for every 'on' input.
 // (Remember that 'off' inputs actually have physical status '1')
 void MFDigInMux::retrigger()
 {
-    // The current state for all attached modules is stored,
+    // The current state is stored,
     // so future update() calls will work off whatever was read by the
     // retrigger flow.
     poll(DONT_TRIGGER); // just read, do not retrigger
