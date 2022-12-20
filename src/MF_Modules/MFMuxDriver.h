bae2fb978365b203b63b63e2f5d626bccbcf8899
GioCC <25667790+GioCC@users.noreply.github.com>
2022-12-20 12:05:20 +0100

    Removed unused functions from MFMuxDriver

--- src/MF_Modules/MFMuxDriver.h
+++ src/MF_Modules/MFMuxDriver.h
@@ -20,22 +20,7 @@ public:
     void detach();
 
     uint8_t setChannel(uint8_t value);
-
-    // Get stored channel number (see comments to RestoreChannel)
     uint8_t getChannel(void) { return _channel; };
-
-    // Increments current channel, wraps around to 0
-    uint8_t nextChannel(void) { return setChannel((++_channel) % 16); }
-
-    // Temporarily stores current channel for later retrieval
-    void saveChannel(void) { _savedChannel = _channel; } // Not reentrant - one level only
-
-    // Restores previously stored channel
-    // >>> This intentionally only restores the channel number, NOT the selector outputs,
-    // in order not to waste time.
-    // A caller will most likely set a new channel right away, and use the stored value
-    // just to determine which channel should be next.
-    void restoreChannel(void) { _channel = _savedChannel; }
 
 private:
     enum { MUX_INITED = 0,
@@ -44,7 +29,6 @@ private:
     uint8_t _selPin[4]; // Selector pins; 0 is LSb
     uint8_t _flags;
     uint8_t _channel;
-    uint8_t _savedChannel;
 };
 
 // MFMuxDriver.h
