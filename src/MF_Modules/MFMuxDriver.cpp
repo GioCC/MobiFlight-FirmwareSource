7f129477bfe3cdd3117771806154307516256d67
GioCC <g.crocic@gmail.com>
2023-03-13 15:32:17 +0100

    Modifications to optimize the switching speed of the Multiplexer selector

--- src/MF_Modules/MFMuxDriver.cpp
+++ src/MF_Modules/MFMuxDriver.cpp
@@ -7,11 +7,39 @@
 #include "mobiflight.h"
 #include "MFMuxDriver.h"
 
+static void directPinMode(uint8_t port, uint8_t pinMsk, uint8_t mode)
+{
+    volatile uint8_t *reg, *out;
+    reg = portModeRegister(port);
+    out = portOutputRegister(port);
+
+    uint8_t oldSREG = SREG;
+    cli();
+    if (mode == INPUT_PULLUP) {
+        *reg &= ~pinMsk;
+        *out |= pinMsk;
+    } else if (mode == OUTPUT) {
+        *reg |= pinMsk;
+    }
+    SREG = oldSREG;
+}
+
+static void directPinOut(uint8_t port, uint8_t pinMsk, uint8_t val)
+{
+    volatile uint8_t *out;
+    out = portOutputRegister(port);
+    uint8_t oldSREG = SREG;
+    cli();
+    (val == LOW ? (*out &= ~pinMsk) : (*out |= pinMsk));
+    SREG = oldSREG;
+}
+
 MFMuxDriver::MFMuxDriver(void)
 {
-    _flags = 0x00;
+    _inited = 0;
     for (uint8_t i = 0; i < 4; i++) {
-        _selPin[i] = 0xFF;
+        _selPort[i]    = 0xFF;
+        _selPinMask[i] = 0xFF;
     }
 }
 
@@ -19,34 +47,38 @@ MFMuxDriver::MFMuxDriver(void)
 void MFMuxDriver::
     attach(uint8_t Sel0Pin, uint8_t Sel1Pin, uint8_t Sel2Pin, uint8_t Sel3Pin)
 {
-    _selPin[0] = Sel0Pin;
-    _selPin[1] = Sel1Pin;
-    _selPin[2] = Sel2Pin;
-    _selPin[3] = Sel3Pin;
-    _flags     = 0x00;
+    _selPort[0]    = digitalPinToPort(Sel0Pin);
+    _selPinMask[0] = digitalPinToBitMask(Sel0Pin);
+    _selPort[1]    = digitalPinToPort(Sel1Pin);
+    _selPinMask[1] = digitalPinToBitMask(Sel1Pin);
+    _selPort[2]    = digitalPinToPort(Sel2Pin);
+    _selPinMask[2] = digitalPinToBitMask(Sel2Pin);
+    _selPort[3]    = digitalPinToPort(Sel3Pin);
+    _selPinMask[3] = digitalPinToBitMask(Sel3Pin);
 
-    for (uint8_t i = 0; i < 4; i++)
-        pinMode(_selPin[i], OUTPUT);
-    bitSet(_flags, MUX_INITED);
-
+    for (uint8_t i = 0; i < 4; i++) {
+        directPinMode(_selPort[i], _selPinMask[i], OUTPUT);
+    }
+        
+    _inited = 1;
     setChannel(0);
 }
 
 void MFMuxDriver::detach()
 {
     for (uint8_t i = 0; i < 4; i++) {
-        if (_selPin[i] == 0xFF) continue;
-        pinMode(_selPin[i], INPUT_PULLUP);
-        _selPin[i] = 0xFF;
+        if (_selPort[i] == 0xFF) continue;
+        directPinMode(_selPort[i], _selPinMask[i], INPUT_PULLUP);
+        _selPort[i]    = 0xFF;
+        _selPinMask[i] = 0xFF;
     }
-    bitClear(_flags, MUX_INITED);
+    _inited = 0;
 }
 
 // Sets the driver lines to select the specified channel
 void MFMuxDriver::setChannel(uint8_t value)
 {
-    if (!bitRead(_flags, MUX_INITED)) return;
-    if (value > 15) return;
+    if (!_inited) return;
 
     // Ideally, setChannel() should change all pins atomically (at the same time):
     // since it doesn't, be advised that there will be signal glitches because
@@ -57,11 +89,12 @@ void MFMuxDriver::setChannel(uint8_t value)
     // However, this effect might have to be taken into account.
 
     _channel = value;
-    for (uint8_t i = 0; i < 4; i++) {
-        digitalWrite(_selPin[i], (value & 0x01));
-        value >>= 1;
-    }
+    directPinOut(_selPort[0], _selPinMask[0], (value & 0x01)!=0);
+    directPinOut(_selPort[1], _selPinMask[1], (value & 0x02)!=0);
+    directPinOut(_selPort[2], _selPinMask[2], (value & 0x04)!=0);
+    directPinOut(_selPort[3], _selPinMask[3], (value & 0x08)!=0);
 }
+
 
 // Returns currently selected channel
 uint8_t MFMuxDriver::getChannel(void)
