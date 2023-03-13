e71b12e6cb6fe709fe1e99e9b4f342d78cd75073
GioCC <g.crocic@gmail.com>
2023-03-13 15:32:17 +0100

    Modifications to optimize the switching speed of the Multiplexer selector

--- src/MF_Modules/MFMuxDriver.h
+++ src/MF_Modules/MFMuxDriver.h
@@ -16,8 +16,8 @@ class MFMuxDriver
 {
 public:
     MFMuxDriver(void);
-    void    attach(uint8_t Sel0Pin, uint8_t Sel1Pin, uint8_t Sel2Pin, uint8_t Sel3Pin);
-    void    detach();
+    void attach(uint8_t Sel0Pin, uint8_t Sel1Pin, uint8_t Sel2Pin, uint8_t Sel3Pin);
+    void detach();
 
     // void setChannelOpt(uint8_t mode);
     void    setChannel(uint8_t value);
@@ -27,11 +27,9 @@ public:
     void    restoreChannel(void);
 
 private:
-    enum { MUX_INITED = 0,
-    };
-
-    uint8_t _selPin[4]; // Selector pins; 0 is LSb
-    uint8_t _flags;
+    uint8_t _selPort[4];    // Selector pins; 0 is LSb
+    uint8_t _selPinMask[4]; // Selector pins; 0 is LSb
+    uint8_t _inited;
     uint8_t _channel;
     uint8_t _savedChannel;
 };
